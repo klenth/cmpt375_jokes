9e2291a045d5daeefc4aeecb0f34c8ccfc042144
Kathy Lenth <klenth@westminstercollege.edu>
2019-10-08 12:52:12 -0600

    Initial commit

---
+++ cmpt375_jokes/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cmpt375_jokes project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmpt375_jokes.settings')
+
+application = get_wsgi_application()
